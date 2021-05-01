e45bdb238457c33d29fbe2151c7580c0db642223
Lucas Rodrigues <lucasrchaves1@gmail.com>
2021-05-01 12:27:47 -0300

    Atualizar estado

--- src/components/github/ReposContainer.js
+++ src/components/github/ReposContainer.js
@@ -9,18 +9,37 @@ class ReposContainer extends Component{
 
         this.state = {
 
-            repositorios: []
+            repositorios: [],
+            username: '',
         }
+
+        this.changeHandler = this.changeHandler.bind(this)
     }
 
     componentDidMount(){
         fetchRepos('lucasrc98').then(res => this.setState({repositorios: res.data}))
     }
 
+    changeHandler(ev){
+        this.setState({repositorios: ev.target.value})
+    }
+
     render(){
         return(
             <div>
                 <h1>Repositórios</h1>
+
+                <form action="#">
+                    <input 
+                        type="search" 
+                        onChange = {this.changeHandler}>
+
+                    </input>
+                    <input type="button" value="" onClick = {}></input>
+                </form>
+
+
+
                 <ReposList repositorios = {this.state.repositorios}></ReposList>
             </div>
         )
